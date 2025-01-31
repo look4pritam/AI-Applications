f1d7689e0f796d2966fdd4adc68be9fcf94a38f1
look4pritam <pritam_shete@yahoo.com>
2025-01-31 19:04:26 +0530

    Update Sample.py

--- Problems/TextAnalytics/Sample.py
+++ Problems/TextAnalytics/Sample.py
@@ -2,8 +2,7 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return n * factorial(n - 2)  # Intentional error: should be (n - 1)
+        return n * factorial(n - 2)
 
 num = int(input("Enter a number: "))
 print(f"Factorial of {num} is {factorial(num)}")
-
